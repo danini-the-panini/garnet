2abf62fc303048530789a1e509332ceb091b95bf
Daniel Smith <jellymann@gmail.com>
2019-10-08 21:31:44 +0200

    run parser

--- lib/ruby_ruby.rb
+++ lib/ruby_ruby.rb
@@ -2,7 +2,13 @@
 
 module RubyRuby
   class Error < StandardError; end
+
+  def self.run(input, filename)
+    tokens = Lexer.new(input, filename).tokens
+    ast = Parser.new(tokens).parse
+  end
 end
 
 require "ruby_ruby/version"
 require "ruby_ruby/lexer"
+require "ruby_ruby/parser"
