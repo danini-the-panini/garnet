529fcf55850728e3a7c046e5c6d1782839d2fe70
Daniel Smith <jellymann@gmail.com>
2019-12-27 16:00:16 +0200

    add: array diff, and, or

--- lib/garnet_ruby/core/array.rb
+++ lib/garnet_ruby/core/array.rb
@@ -28,7 +28,6 @@ module GarnetRuby
 
   module Core
     class << self
-
       def ary_inspect(ary)
         strings = ary.array_value.map do |item|
           rb_funcall(item, :to_s).string_value
@@ -60,6 +59,14 @@ module GarnetRuby
         when 3
           ary.array_value[args[0].value, args[1].value] = args[2]
         end
+      end
+
+      def ary_includes(ary, item)
+        ary.array_value.any? { |e| rb_equal(item, e) } ? Q_TRUE : Q_FALSE
+      end
+
+      def ary_includes_by_eql(ary, item)
+        ary.array_value.any? { |e| rb_eql(item, e) } ? Q_TRUE : Q_FALSE
       end
 
       def ary_join(ary, sep)
@@ -112,6 +119,56 @@ module GarnetRuby
         end
         RPrimitive.from(h)
       end
+
+      def ary_diff(ary1, ary2)
+        ary2 = ary2.to_array_type
+        ary3 = RArray.from([])
+
+        ary1.array_value.each do |elt|
+          next if ary_includes_by_eql(ary2, elt)
+
+          ary3.array_value.push(elt)
+        end
+        
+        # TODO: use a hash for big arrays
+
+        ary3
+      end
+
+      def ary_and(ary1, ary2)
+        ary2 = ary2.to_array_type
+        ary3 = RArray.from([])
+        return ary3 if ary1.array_value.empty? || ary2.array_value.empty?
+
+        ary1.array_value.each do |v|
+          next if !ary_includes_by_eql(ary2, v)
+          next if ary_includes_by_eql(ary3, v)
+          ary3.array_value.push(v)
+        end
+        
+        # TODO: use a hash for big arrays
+
+        ary3
+      end
+
+      def ary_or(ary1, ary2)
+        ary2 = ary2.to_array_type
+        ary3 = RArray.from([])
+
+        ary_union(ary3, ary1)
+        ary_union(ary3, ary2)
+
+        # TODO: use a hash for big arrays
+
+        ary3
+      end
+
+      def ary_union(ary_union, ary)
+        ary.array_value.each do |elt|
+          next if ary_includes_by_eql(ary_union, elt)
+          ary_union.array_value.push(elt)
+        end
+      end
     end
 
     def self.init_array
@@ -127,6 +184,10 @@ module GarnetRuby
 
       rb_define_method(cArray, :+, &method(:ary_plus))
       rb_define_method(cArray, :*, &method(:ary_times))
+      
+      rb_define_method(cArray, :-, &method(:ary_diff))
+      rb_define_method(cArray, :&, &method(:ary_and))
+      rb_define_method(cArray, :|, &method(:ary_or))
     end
   end
 end
