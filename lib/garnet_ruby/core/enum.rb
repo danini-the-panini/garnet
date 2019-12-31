40da9b5c7049ff6d21d8518595a92b0b47ff91af
Daniel Smith <jellymann@gmail.com>
2019-12-31 15:09:16 +0200

    add: enumerable collect

--- lib/garnet_ruby/core/enum.rb
+++ lib/garnet_ruby/core/enum.rb
@@ -10,12 +10,28 @@ module GarnetRuby
 
         ary
       end
+
+      def enum_collect(obj)
+        # TODO: return enumerator
+
+        vm = VM.instance
+        block = vm.caller_environment.block
+
+        ary = RArray.from([])
+        rb_block_call(obj, :each) do |x|
+          ary.array_value.push(vm.execute_block(block, [x]))
+        end
+
+        ary
+      end
     end
 
     def self.init_enum
       @mEnumerable = rb_define_module(:Enumerable)
 
       rb_define_method(mEnumerable, :to_a, &method(:enum_to_a))
+
+      rb_define_method(mEnumerable, :collect, &method(:enum_collect))
     end
   end
 end
