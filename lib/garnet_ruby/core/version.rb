1ce1e7bbb42435a52a050038bbe4488c8a537814
Daniel Smith <jellymann@gmail.com>
2020-01-18 18:56:21 +0200

    add: RUBY_PLATFORM

--- lib/garnet_ruby/core/version.rb
+++ lib/garnet_ruby/core/version.rb
@@ -2,6 +2,7 @@ module GarnetRuby
   module Core
     def self.init_version
       rb_define_global_const(:RUBY_VERSION, RString.from(EQ_RUBY_VERSION))
+      rb_define_global_const(:RUBY_PLATFORM, RString.from("ruby-#{RUBY_PLATFORM}"))
       rb_define_global_const(:RUBY_ENGINE, RString.from(ENGINE_NAME))
       rb_define_global_const(:RUBY_ENGINE_VERSION, RString.from(VERSION))
     end
