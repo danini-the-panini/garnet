539d7d47ba27d4cb716a9de7d014b2ce64974622
Daniel Smith <jellymann@gmail.com>
2020-01-18 18:53:42 +0200

    add: Dir.delete

--- lib/garnet_ruby/core/dir.rb
+++ lib/garnet_ruby/core/dir.rb
@@ -1,12 +1,29 @@
 module GarnetRuby
   module Core
     class << self
+      def check_dirname(dir)
+        # TODO
+        rb_get_path(dir)
+      end
+
       def dir_s_getwd(_)
         RString.from(Dir.pwd)
       end
 
+      def dir_s_rmdir(obj, dir)
+        dir = check_dirname(dir)
+
+        Dir.delete(dir.string_value)
+
+        RPrimitive.from(0)
+      rescue SystemCallError => e
+        rb_raise(@syserr_tbl[e.errno], e.message)
+      end
+
       def dir_s_aref(_, *args)
         RArray.from(Dir[*args.map{ |s| s.obj_as_string.string_value }])
+      rescue SystemCallError => e
+        rb_raise(@syserr_tbl[e.errno], e.message)
       end
     end
 
@@ -14,6 +31,7 @@ module GarnetRuby
       @cDir = rb_define_class(:Dir, cObject)
 
       rb_define_singleton_method(cDir, :pwd, &method(:dir_s_getwd))
+      rb_define_singleton_method(cDir, :delete, &method(:dir_s_rmdir))
 
       rb_define_singleton_method(cDir, :[], &method(:dir_s_aref))
     end
