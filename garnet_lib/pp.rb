5a0beb548615a525d7f68df19978d1d38af43d32
Daniel Smith <jellymann@gmail.com>
2020-01-18 17:18:38 +0200

    add: built-in libs for garnet

---
+++ garnet_lib/pp.rb
@@ -0,0 +1,16 @@
+# require 'prettyprint'
+
+# TODO
+
+module Kernel
+  def pretty_inspect
+    inspect
+  end
+
+  def pp(*objs)
+    objs.each do |obj|
+      p obj
+    end
+    objs.size <= 1 ? objs.first : objs
+  end
+end
